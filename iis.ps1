39661a4fbb67003c22a0909f1478af82ab2f4d33
sanjayntms <sanjayntms@github.com>
2020-12-08 18:53:54 +0530

    s

--- iis.ps1
+++ iis.ps1
@@ -1,7 +1,7 @@
 Add-WindowsFeature Web-Server -IncludeManagementTools
 Add-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "NTMS web server --> $($env:computername)"
 New-Item -ItemType directory -Path "C:\inetpub\wwwroot\images"
-New-Item -ItemType directory -Path "C:\inetpub\wwwroot\video"
+New-Item -ItemType directory -Path "C:\inetpub\wwwroot\videos"
 $imagevalue = "Images: " + $($env:computername)
 Add-Content -Path "C:\inetpub\wwwroot\images\test.htm" -Value $imagevalue
 $videovalue = "Video: " + $($env:computername)
